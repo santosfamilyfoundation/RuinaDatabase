8d7efe175fc8517dd2d27f00e33bda1e39881985
Enmoren <enmo.ren@students.olin.edu>
2020-11-07 02:50:08 +0800

    Fix some documentation

--- AWSdeployFlaskBackendGuide.md
+++ AWSdeployFlaskBackendGuide.md
@@ -1,6 +1,6 @@
 # AWS EC2 Setup Guide
 
-## Reference this [link](https://www.twilio.com/blog/deploy-flask-python-app-aws#:~:text=A%20GitHub%20repository%20with%20files,starting%20a%20basic%20Flask%20app.) for deploying a backend in AWS EC2 instance.
+Reference this [link](https://www.twilio.com/blog/deploy-flask-python-app-aws#:~:text=A%20GitHub%20repository%20with%20files,starting%20a%20basic%20Flask%20app.) for deploying a backend in AWS EC2 instance.
 
 ## To transfer your project files to remote host
 1. Use "ruina-db-test.pem" located inside of this project folder
--- base.py
+++ base.py
@@ -9,19 +9,17 @@ from wtforms import Form
 from libs.err_code import ParameterException
 
 
-# TODO 获取数据 抛出异常
+# TODO
 class BaseForm(Form):
     def __init__(self):
         # data = request.json
-        # 静默模式，意思说不要报错
         data = request.get_json(silent=True)
-        args = request.args.to_dict()  # get请求参数的时候，配合下面**args将get请求参数传过去
+        args = request.args.to_dict()
         super(BaseForm, self).__init__(data=data, **args)
 
-    # 实现了在发生错误的时候会打印json格式的错误信息
     def validate_for_api(self):
         valid = super(BaseForm, self).validate()
         if not valid:
-            # 所有的错误信息都在Form.errors中
+            # error msg will be in form.errors
             raise ParameterException(msg=self.errors)
-        return self  # 把form返回回去
+        return self  # send form back
